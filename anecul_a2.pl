ac4c2c4dd7609a933abd601bb88044639d0e584f
Amos Madalin Neculau <contact@amosneculau.ro>
2018-04-19 16:02:39 +0200

    Update anecul_a2.pl

--- anecul_a2.pl
+++ anecul_a2.pl
@@ -21,20 +21,23 @@ attend(S, C).
 /* Implementation of Level 1 */
 
 pass1(X):- student(X, K), has_credits(X,E), E @>= 30.
-fail1(X):- student(X, K), has_credits(X,E), E @< 30.
+fail1(X):- \+pass1(X).
 
-has_credits(X,E).
+has_credits(X,E):-student(X,K).
 
-/* Amos tests:
--------------------------
+/*
+Amos' tests
 student(amos, ss).
 student(sam, ss).
 student(bob, ss).
-student(alice, ifi).
+student(rob, ss)
+
 has_credits(amos, 30).
 has_credits(sam, 10).
 has_credits(alice, 200).
--------------------------
+has_credits(bob, 30).
+has_credits(rob, 29).
+has_credits(ben, 100).
 */
 
 
